77ae6278267d035fac1b459bf4f084fda4e8d8e6
Sweets <sweetmantech@gmail.com>
2020-03-16 18:31:03 -0400

    I add Proposal to website

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -47,33 +47,44 @@ export class Home extends Component {
                                 </h1>
                             </div>
                         </Cell>
-                        <Cell col={4}>
+                        <Cell col={4} tablet={12} phone={12}>
+                            <Card shadow={0} style={{ margin: 'auto' }}>
+                                <CardTitle>#canstopcolumbus Proposal</CardTitle>
+                                <CardText>
+                                On Sunday night, the Columbus tech and business community started talking about the idea of a virtual hackathon in response to the Covid-19 situation. With social distancing, no school, remote work, and a sense of urgency, there’s a lot of energy focused on helping. We proposed an online hacking event where anyone can contribute by defining challenges, building projects, and sharing resources.
+                                </CardText>
+                                <CardActions border>
+                                    <a href = "https://docs.google.com/document/d/1rWjqOmbKsfSFHJaNzsz9JLa50fskQ8cCcmPa5T40HRs/edit?usp=sharing" target="_blank" rel="noopener noreferrer"><Button ripple accent raised >Get Started</Button></a>
+                                </CardActions>
+                            </Card>
+                        </Cell>
+                        <Cell col={4} tablet={12} phone={12}>
                             <Card shadow={0} style={{ margin: 'auto' }}>
                                 <CardTitle> Planning Doc</CardTitle>
                                 <CardText>
                                     Primary for ALL hackathon resources. Includes: Planning, Team Formation, Ideas, etc to fight COVID-19.
                                 </CardText>
                                 <CardActions border>
-                                    <a href = "https://docs.google.com/spreadsheets/d/16rHl_qXc1wp8vSg_Ux8YXdYe_F_B88ghLW7ese2YFec/edit?usp=sharing"><Button ripple accent raised >Get Started</Button></a>
+                                    <a href = "https://docs.google.com/spreadsheets/d/16rHl_qXc1wp8vSg_Ux8YXdYe_F_B88ghLW7ese2YFec/edit?usp=sharing" target="_blank" rel="noopener noreferrer"><Button ripple accent raised >Get Started</Button></a>
                                 </CardActions>
                             </Card>
                         </Cell>
-                        <Cell col={4}>
+                        <Cell col={4} tablet={12} phone={12}>
                             <Card shadow={0} style={{ margin: 'auto' }}>
                                 <CardTitle> Slack Channel</CardTitle>
                                 <CardText>
                                     Chat for ALL hackathon communications. Check it out, collaborate, form a team, and save the day from COVID-19.
                                 </CardText>
                                 <CardActions border>
-                                    <a href = "https://app.slack.com/client/T0101PN34J1/"><Button ripple accent raised >Get Started</Button></a>
+                                    <a href = "https://app.slack.com/client/T0101PN34J1/" target="_blank" rel="noopener noreferrer"><Button ripple accent raised >Get Started</Button></a>
                                 </CardActions>
                             </Card>
                         </Cell>
-                        <Cell col={4}>
+                        <Cell col={12} >
                             <Card shadow={0} style={{ margin: 'auto' }}>
                                 <CardTitle> Spread the Word</CardTitle>
                                 <CardText>
-                                    Twitter is Earth's realtime communication channel. One tweet is all it takes to spread awareness and fight COVID-19.
+                                    Twitter is Earth's realtime communication channel. One tweet is all it takes to spread awareness and fight COVID-19. #cantstopcolumbus 
                                 </CardText>
                                 <CardActions border>
                                     <TwitterShareButton url="https://cantstopcolumbus.web.app/" children={<Button raised accent ripple>Tweet</Button>} title={"Want to help Columbus fight rapid-response against COVID-19? Join the Can't Stop Columbus Hackathon."} hashtags={["COVID19OhioReady", "cantstopcolumbus"]}></TwitterShareButton>
